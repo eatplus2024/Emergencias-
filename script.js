e95c0c33ff868276514fd86ae9f9ff0b9e59de21
eatplus2024 <edwincito2021@gmail.com>
2025-04-03 23:05:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const llamarBtns = document.querySelectorAll('.llamar-btn');
+const compartirBtns = document.querySelectorAll('.compartir-btn');
 
 llamarBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const numero = btn.dataset.numero;
         window.location.href = `tel:${numero}`;
+    });
+});
+
+compartirBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const numero = btn.dataset.numero;
+        const mensaje = `Llama a este número de emergencia: ${numero}`;
+        const url = `whatsapp://send?text=${encodeURIComponent(mensaje)}`;
+        window.location.href = url;
     });
 });
