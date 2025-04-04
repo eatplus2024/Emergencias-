1e2f820ca4f1f18a1e38d2cd7d08f997e7b8a071
eatplus2024 <edwincito2021@gmail.com>
2025-04-03 23:19:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const llamarBtns = document.querySelectorAll('.llamar-btn');
 const compartirBtns = document.querySelectorAll('.compartir-btn');
-
-llamarBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const numero = btn.dataset.numero;
-        window.location.href = `tel:${numero}`;
-    });
-});
 
 compartirBtns.forEach(btn => {
     btn.addEventListener('click', () => {
